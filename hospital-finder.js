889b801dde55bb86d466920c3948814b013cfc94
Sangavi-png <palanisamysangavi5@gmail.com>
2025-08-18 20:34:38 +0530

    Update hospital-finder.js

--- hospital-finder.js
+++ hospital-finder.js
@@ -10,4 +10,29 @@
 </select>
 
 <br><br>
-<button onclick>
+<button onclick="showHospitals()">Find Hospitals</button>
+
+<div id="hospitalResults"></div>
+
+<script>
+  const hospitalSuggestions = {
+    breast: ["Tata Memorial Hospital, Mumbai", "AIIMS, Delhi"],
+    lung: ["Apollo Hospitals, Chennai", "Fortis Memorial, Gurgaon"],
+    colon: ["Christian Medical College, Vellore", "Kokilaben Hospital, Mumbai"],
+    leukemia: ["Rajiv Gandhi Cancer Institute, Delhi", "Kidwai Memorial, Bangalore"]
+  };
+
+  function showHospitals() {
+    const type = document.getElementById("cancerType").value;
+    const resultsDiv = document.getElementById("hospitalResults");
+
+    if (hospitalSuggestions[type]) {
+      const list = hospitalSuggestions[type]
+        .map(hospital => `<li>${hospital}</li>`)
+        .join("");
+      resultsDiv.innerHTML = `<ul>${list}</ul>`;
+    } else {
+      resultsDiv.innerHTML = "Please select a valid cancer type.";
+    }
+  }
+</script>
